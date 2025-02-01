96fb9dc8aa9b67c93c8afb3869ea8240495c00af
Evangelos Pittas <evangelospittas@gmail.com>
2025-02-01 13:45:32 +0200

    feat: init xcode proj

---
+++ DemoPokedex/DemoPokedex/DemoPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoPokedexApp.swift
+//  DemoPokedex
+//
+//  Created by Evangelos Pittas on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
